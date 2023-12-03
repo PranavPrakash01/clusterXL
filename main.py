79d89cd3d7f1226ed775b8822a052ec1fb60373b
Pranav Prakash <pranavpprakash7@gmail.com>
2023-12-03 19:57:05 +0530

    Add table and operation buttons

--- main.py
+++ main.py
@@ -1,70 +1,89 @@
 # main.py
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView, QPushButton, QVBoxLayout, QWidget
 from PyQt5.QtCore import Qt
 from nodes import Node, NodeGraph
 from node_widget import NodeWidget
 from operation_nodes import OperationNode
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Create the main window
+        self.setWindowTitle("Node-based Spreadsheet")
+        self.setGeometry(100, 100, 800, 600)
+
+        # Create a central widget and layout
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        layout = QVBoxLayout(central_widget)
+
+        # Create a QGraphicsScene
+        self.scene = QGraphicsScene()
+        self.scene.setSceneRect(-400, -300, 800, 600)
+
+        # Create a QGraphicsView
+        self.view = QGraphicsView(self.scene)
+        self.view.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        layout.addWidget(self.view)
+
+        # Create table nodes
+        self.table_nodes = []
+
+        # Create operation nodes
+        self.operation_nodes = []
+
+        # Create buttons
+        self.addButton = QPushButton("Add Table", self)
+        self.addButton.clicked.connect(self.add_table_node)
+        layout.addWidget(self.addButton)
+
+        self.addOperationButton = QPushButton("Add Operation", self)
+        self.addOperationButton.clicked.connect(self.add_operation_node)
+        layout.addWidget(self.addOperationButton)
+
+        # Create a graph instance
+        self.graph = NodeGraph()
+
+        # Show the main window
+        self.show()
+
+    def add_table_node(self):
+        # Create a new table node
+        table_node = Node(node_id=len(self.table_nodes) + 1, data={"type": "Table"})
+        self.table_nodes.append(table_node)
+
+        # Add table node to the graph
+        self.graph.add_node(table_node)
+
+        # Create a NodeWidget and add it to the scene
+        node_widget = NodeWidget(table_node, width=50, height=30)
+        self.scene.addItem(node_widget)
+
+        # Position the node in the scene
+        node_widget.setPos(0, 0)
+
+    def add_operation_node(self):
+        # Create a new operation node
+        operation_node = OperationNode(operation="Addition")
+        self.operation_nodes.append(operation_node)
+
+        # Add operation node to the scene
+        self.scene.addItem(operation_node)
+
+        # Position the node in the scene
+        operation_node.setPos(0, 0)
 
 def main():
     # Initialize the application
     app = QApplication([])
 
     # Create the main window
-    window = QMainWindow()
-    window.setWindowTitle("Node-based Spreadsheet")
-    window.setGeometry(100, 100, 800, 600)
-
-    # Create a QGraphicsScene
-    scene = QGraphicsScene()
-    scene.setSceneRect(-400, -300, 800, 600)
-
-    # Create a QGraphicsView
-    view = QGraphicsView(scene)
-    view.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-
-    # Set up the main window layout
-    layout = QMainWindow()
-    layout.setCentralWidget(view)
-
-    # Create table nodes
-    table_node1 = Node(node_id=1, data={"type": "Table"})
-    table_node2 = Node(node_id=2, data={"type": "Table"})
-
-    # Create operation nodes
-    addition_node = OperationNode(operation="Addition")
-    subtraction_node = OperationNode(operation="Subtraction")
-
-    # Add nodes to the graph
-    graph = NodeGraph()
-    graph.add_node(table_node1)
-    graph.add_node(table_node2)
-
-    # Connect nodes
-    graph.connect_nodes(table_node1, table_node2)
-
-    # Add table nodes to the scene
-    node_widget1 = NodeWidget(table_node1, width=50, height=30)
-    node_widget2 = NodeWidget(table_node2, width=50, height=30)
-    scene.addItem(node_widget1)
-    scene.addItem(node_widget2)
-
-    # Add operation nodes to the scene
-    scene.addItem(addition_node)
-    scene.addItem(subtraction_node)
-
-    # Position nodes in the scene
-    node_widget1.setPos(-200, 0)
-    node_widget2.setPos(200, 0)
-    addition_node.setPos(0, -200)
-    subtraction_node.setPos(0, 200)
-
-    # Show the main window
-    window.setCentralWidget(layout)
-    window.show()
+    main_window = MainWindow()
 
     # Start the application event loop
     app.exec_()
 
 if __name__ == "__main__":
     main()
-    
