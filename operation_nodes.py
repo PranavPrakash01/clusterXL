f1f5c78d7d2f50cf1668373c3784d04304b9d844
Pranav Prakash <pranavpprakash7@gmail.com>
2023-12-09 22:04:12 +0530

    average_widget

--- operation_nodes.py
+++ operation_nodes.py
@@ -1,22 +1,34 @@
-# operation_nodes.py
-from PyQt5.QtWidgets import QMenu, QAction
+# operations.py
+from PyQt5.QtWidgets import QMenu, QAction, QGraphicsProxyWidget
+from average_node import AverageNode
 
 def create_operation_menu(main_window):
     # Create a menu
     menu = QMenu(main_window)
 
     # Create actions for single input operations
-    single_input_menu = menu.addAction("Single Input Operations")
+    single_input_menu = menu.addMenu("Single Input Operations")
+    average_action = single_input_menu.addAction("Average")
+    average_action.triggered.connect(lambda: add_average_node(main_window))
 
     # Create actions for double input operations
-    double_input_menu = menu.addAction("Double Input Operations")
+    double_input_menu = menu.addMenu("Double Input Operations")
+    # Add your double input actions here (e.g., RSum, RProduct, RDifference)
 
     # Create actions for multi input operations
-    multi_input_menu = menu.addAction("Multi Input Operations")
+    multi_input_menu = menu.addMenu("Multi Input Operations")
+    # Add your multi input actions here (e.g., RSum, RProduct, RDifference)
+
+    # ... (add more actions as needed)
 
     # Show the menu at the cursor position
-    action = menu.exec_(main_window.cursor().pos())
+    menu.exec_(main_window.cursor().pos())
 
-    # Add logic here to handle the selected action
-    if action:
-        print(f"Selected operation: {action.text()}")
+def add_average_node(main_window):
+    average_node = AverageNode()
+
+    # Create a proxy widget for AverageNode
+    proxy_widget = QGraphicsProxyWidget()
+    proxy_widget.setWidget(average_node)
+
+    main_window.scene.addItem(proxy_widget)
