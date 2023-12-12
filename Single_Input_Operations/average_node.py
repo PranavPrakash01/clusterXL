d2f654da0ca0facb786874702c9a33177e901e6b
Pranav Prakash <pranavpprakash7@gmail.com>
2023-12-12 18:28:31 +0530

    file_rearrage

--- Single_Input_Operations/average_node.py
+++ Single_Input_Operations/average_node.py
@@ -1,7 +1,7 @@
 # average_node.py
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QMenu
 from PyQt5.QtCore import Qt, QPoint
-from .connection_point import ConnectionPoint
+from connection_point import ConnectionPoint
 
 
 class AverageNode(QWidget):
